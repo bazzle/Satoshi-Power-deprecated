d9715126f72ab51e722f8da341ac0027f910df7b
Barry Richards <barry@br-web.me>
2025-02-28 22:32:54 +0000

    Fix hungarian unit bug

--- src/js/DataFetchDisplay.js
+++ src/js/DataFetchDisplay.js
@@ -46,7 +46,12 @@ async function DataFetchDisplay(){
 			itemObj.satPrice = itemObj.btcPrice / 100000000
 
 			// If the currency doesn't have a subunit, the display name and price is the main unit
-			if(itemObj.subUnits === null || itemObj.subUnitName === null){
+			console.log(itemObj);
+			if(
+				itemObj.subUnits === 1 ||
+				itemObj.subUnitName === null ||
+				itemObj.subUnitName === ""
+			){
 				// Set display price, percentage and name which will display on the front-end
 				itemObj.displayPrice = itemObj.satPrice
 				itemObj.percentage = Utilities.getPercentage(itemObj.displayPrice)
@@ -54,11 +59,16 @@ async function DataFetchDisplay(){
 				itemObj.displayName = itemObj.unitName
 			} else {
 				// As the sub unit name isn't prefixed by the country, add it here
-				let namePrefix = Utilities.removeLastWord(itemObj.unitName)
+				let namePrefix
+				namePrefix = Utilities.removeLastWord(itemObj.unitName)
 				if (namePrefix === ""){
 					namePrefix = itemObj.unitName
 				}
-				itemObj.subUnitName = `${namePrefix} ${itemObj.subUnitName}`
+				// Some fixes
+				if(namePrefix.includes('Renminbi')){
+					namePrefix = namePrefix.replace('Renminbi', '');
+				}
+				itemObj.subUnitName = `${namePrefix ? namePrefix : ''} ${itemObj.subUnitName}`
 				// The display price will be the satprice multiplied by how many subunits make up the unit
 				itemObj.displayPrice = itemObj.satPrice * itemObj.subUnits
 				// Set percentage and if the percentage is over 200, we will show the main unit on the front-end
@@ -73,13 +83,11 @@ async function DataFetchDisplay(){
 					itemObj.displayPercentage = Utilities.getPercentage(itemObj.displayPrice)
 				}
 			}
-
+			
 			currenciesArr.push(itemObj);
 		}
 
 		currenciesArr.sort((a, b) => a.displayPercentage - b.displayPercentage);
-
-		console.log(currenciesArr);
 		
 		
 		DomAddCurrency(currenciesArr);
