a99404c7d6d9ce7fb6d90a366d29b4326ddc2d6a
Barry Richards <barry@br-web.me>
2025-03-23 00:24:03 +0000

    Update build script

--- rollup.config.prod.mjs
+++ rollup.config.prod.mjs
@@ -1,14 +1,16 @@
 import sass from 'rollup-plugin-sass';
 import terser from '@rollup/plugin-terser';
 import copy from 'rollup-plugin-copy';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-    input: 'assets/js/main.js',
+    input: 'src/index.ts',
     output: {
         file: 'dist/assets/js/main.js',
         format: 'iife', // Suitable for <script> tags in browsers
     },
     plugins: [
+		typescript(),
         sass({ output: 'dist/assets/styles/main.css' }),
         terser(),
         copy({
